d268c3fcfeeb46d6bf1679b3271417d896322c4d
Moya, Claudio <claudio.moya@rakuten.com>
2018-07-20 17:36:25 +0900

    Update - Webpack now supports the multiple entry points to generate separated builds Css files

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,7 +3,24 @@ const common = require('./webpack.config.common');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const entry = {
-  'rex': './src/themes/default'
+  'rex': './src/rex.scss',
+  'rex-core': './src/rex-core.scss',
+  'rex-core-webfonts': './src/rex-core-webfonts.scss',
+  'rex-grid-responsive': './src/rex-grid-responsive.scss',
+  'rex-grid-mobile': './src/rex-grid-mobile.scss',
+  'rex-grid-non-mobile': './src/rex-grid-non-mobile.scss',
+  'rex-icons-cdn': './src/rex-icons-cdn.scss',
+  'rex-icons': './src/rex-icons.scss',
+  'rex-typography': './src/rex-typography.scss',
+  'rex-tables': './src/rex-tables.scss',
+  'rex-buttons': './src/rex-buttons.scss',
+  'rex-button-groups': './src/rex-button-groups.scss',
+  'rex-forms': './src/rex-forms.scss',
+  'rex-component/lists': './src/rex-component-lists.scss',
+  'rex-component/alerts': './src/rex-component-alerts.scss',
+  'rex-component/breadcrumbs': './src/rex-component-breadcrumbs.scss',
+  'rex-component/pagination': './src/rex-component-pagination.scss',
+  'rex-component/track': './src/rex-component-track.scss',
 };
 
 const plugins = [
